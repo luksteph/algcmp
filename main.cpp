59aa8365f25f8c1dbd6949018089155ca3d44168
luksteph <131851172+luksteph@users.noreply.github.com>
2023-04-26 22:08:38 -0400

    Fleshed out ux/ui more

--- main.cpp
+++ main.cpp
@@ -1,60 +1,137 @@
 #include <vector>
 #include <iostream>
 #include <iomanip>
-#include <cmath>
 #include "algos.h"
 #include "graph_util.h"
-#define INF INT_MAX // change back
 using namespace std;
 
-void test_cases();
+struct userInput {
+    char c, i, a, n;
+    short short int s;
+};
+void welcome();
+void test_cases(); // temp
+void input_handler();
 
 int main() {
     test_cases();
-
-    char c = 0;
-    enum a {INIT, INPUT_TYPE, ALGO_TYPE, NEXT_ACTION, EXIT};
-    while (c != 'e') {
-        switch (c) {
+    
+    userInput input;
+    enum a {INIT, INPUT_TYPE, ALGO_TYPE, NEXT_ACTION, EXIT, CONFIRM_EXIT};
+    
+    input.c = INIT;
+    while (input.c != CONFIRM_EXIT) {
+        switch (input.c) {
             case INIT: {
-                cout << "Graph Node Search" << '\n';
-                c = INPUT_TYPE;
+                welcome();
+                cout << "Welcome! Please follow the prompts to compare different graph traversal algorithms." << '\n';
+                cout << "(You may use 'x' at any prompt to go back.)" << '\n';
+                input.c = INPUT_TYPE;
                 break;
             }
 
             case INPUT_TYPE: {
-                cout << "Generate or input a graph: ";
-                cin >> c;
-                if (c == 'i') cout << "input mode selected";
-                if (c == 'g') cout << "generate mode selected";
+                cout << "Would you like to generate or input a graph?\n"
+                cout << "i = Input Your Own\n" << "g = Generate a Graph\n";
+                cout << "> ";
+                cin >> input.i;
+                if (toLower(input.i) == 'i') cout << "Input mode selected!";
+                else if (toLower(input.i) == 'g') cout << "Generate mode selected!";
+                else if (toLower(input.i) == 'x') input.c = EXIT;
+                else {
+                    cout << "Invalid input type selection.\n"
+                    input.c = INPUT_TYPE; // may be redundant
+                    input.i = '\0'; // reset
+                    break;
+                }
+                input.c = ALGO_TYPE;
                 break;
             }
             case ALGO_TYPE: {
-                cout << "Algorithm to use: ";
-                cin >> c;
-                if (c == 'd') cout << "Djikstras alg selected";
-                if (c == 'a') cout << "A* selected";
-                if (c == 'f') cout << "Bellman-Ford alg selected";
-                if (c == 'b') cout << "bidirectional search selected";
+                cout << "Which algorithm would you like to use?\n";
+                cout << "d = Dijkstra's\n" << "a = A*\n" << "b = Bellman-Ford\n";
+                cout << "> ";
+                cin >> input.a;
+                switch (toLower(input.a)) {
+                    case 'd': {
+                        cout << "Dijkstra's selected!\n";
+                        input_handler(input);
+                        break;
+                    }
+                    case 'a': {
+                        cout << "A* selected!\n";
+                        input_handler(input);
+                        break;
+                    }
+                    case 'b': {
+                        cout << "Bellman-Ford selected!\n";
+                        input_handler(input);
+                        break;
+                    }
+                    case 'x': {
+                        input.c = INPUT_TYPE;
+                        input.a = '\0'; // reset algo choice
+                        break;
+                    }
+                    default: {
+                        cout << "Invalid algorithm selection.\n"
+                        input.c = ALGO_TYPE;
+                        input.a = '\0'; // reset algo choice
+                        break;
+                    }
+                }
                 break;
             }
             case NEXT_ACTION: {
-                cout << "Next action; g for new graph, a for new algo, e to exit";
-                cin >> c;
-                if (c == 'g') c = INPUT_TYPE;
-                if (c == 'a') c = ALGO_TYPE;
-                if (c == 'e') c = EXIT;
+                cout << "What would you like to do next?\n";
+                cout << "g = Change the Graph\n" << "a = Choose an Algorithm\n" << "x = Exit\n";
+                cout << "> ";
+                cin >> input.n;
+                if (input.n == 'g') input.c = INPUT_TYPE;
+                else if (input.n == 'a') input.c = ALGO_TYPE;
+                else if (input.n == 'x') input.c = EXIT;
+                else {
+                    cout << "Invalid action.\n"
+                    input.c = NEXT_ACTION; // may be redundant
+                    input.n = '\0'; // reset
+                    break;
+                }
                 break;
             }
             case EXIT: {
-                cout << "Exiting!";
+                cout << "Type x to confirm exit, or any other character to return.\n";
+                cout << "> ";
+                cin >> c;
+                if (toLower(c) == 'x') {
+                    input.c = CONFIRM_EXIT;
+                }
+                else {
+                    input.c = NEXT_ACTION;
+                }
                 break;
             }
-            default:
+            default: cout << "default flag";
                 break;
         }
     }
     return 0;
+}
+
+void welcome() {
+    cout <<
+    "───────────────────────────────────────────────────────────────────────────────────────────────────\n" <<
+    "─██████████████─██████─────────██████████████─██████████████─██████──────────██████─██████████████─\n" <<
+    "─██░░░░░░░░░░██─██░░██─────────██░░░░░░░░░░██─██░░░░░░░░░░██─██░░██████████████░░██─██░░░░░░░░░░██─\n" <<
+    "─██░░██████░░██─██░░██─────────██░░██████████─██░░██████████─██░░░░░░░░░░░░░░░░░░██─██░░██████░░██─\n" <<
+    "─██░░██──██░░██─██░░██─────────██░░██─────────██░░██─────────██░░██████░░██████░░██─██░░██──██░░██─\n" <<
+    "─██░░██████░░██─██░░██─────────██░░██─────────██░░██─────────██░░██──██░░██──██░░██─██░░██████░░██─\n" <<
+    "─██░░░░░░░░░░██─██░░██─────────██░░██──██████─██░░██─────────██░░██──██░░██──██░░██─██░░░░░░░░░░██─\n" <<
+    "─██░░██████░░██─██░░██─────────██░░██──██░░██─██░░██─────────██░░██──██████──██░░██─██░░██████████─\n" <<
+    "─██░░██──██░░██─██░░██─────────██░░██──██░░██─██░░██─────────██░░██──────────██░░██─██░░██─────────\n" <<
+    "─██░░██──██░░██─██░░██████████─██░░██████░░██─██░░██████████─██░░██──────────██░░██─██░░██─────────\n" <<
+    "─██░░██──██░░██─██░░░░░░░░░░██─██░░░░░░░░░░██─██░░░░░░░░░░██─██░░██──────────██░░██─██░░██─────────\n" <<
+    "─██████──██████─██████████████─██████████████─██████████████─██████──────────██████─██████─────────\n" <<
+    "───────────────────────────────────────────────────────────────────────────────────────────────────\n";
 }
 
 void test_cases() {
@@ -67,4 +144,21 @@ void test_cases() {
     myFirstGraph.print_graph();
     myFirstGraph.adjmtx_rand(3);
     myFirstGraph.print_graph();
+}
+                    
+void input_handler(userInput input) {
+    cout << input.i << input.a; // c, n is unused in this handler
+
+    if (input.i == 'g') {
+        cout << "Input a number of vertices for the graph [1, 20].";
+        cout << "Any values outside this range will randomize the size of the graph within [1, 20]."
+        //handle that.
+    }
+    else {
+        cout << "Input a number of vertices for the graph [1, 20].";
+        //validate that
+        cin >> input.s;
+        Graph g(input.s);
+        
+    }
 }
